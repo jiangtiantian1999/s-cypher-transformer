0d590fc36337d9c7d20cfb597e2f0e8a57444ef6
Jiangtiantian1999 <jiangtiantian1999@163.com>
2023-08-08 11:37:53 +0800

    init

--- transformer/ir/SNode.py
+++
@@ -1,7 +0,0 @@
-class SNode:
-
-    def __init__(self, node_id, node_type, content, interval):
-        self.node_id = node_id
-        self.node_type = node_type  # OBJECT or PROPERTY or VALUE
-        self.content = content
-        self.interval = interval
