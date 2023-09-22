aae81009d39775138b90a58e4c363c2055c321a8
lingyaolu <lingyaoluu@163.com>
2023-09-22 10:36:27 +0800

    fix bug

--- transformer/grammar_parser/s_cypherListener.py
+++ transformer/grammar_parser/s_cypherListener.py
@@ -1,6 +1,6 @@
-# Generated from .\s_cypher.g4 by ANTLR 4.9.2
+# Generated from .\s_cypher.g4 by ANTLR 4.13.0
 from antlr4 import *
-if __name__ is not None and "." in __name__:
+if "." in __name__:
     from .s_cypherParser import s_cypherParser
 else:
     from s_cypherParser import s_cypherParser
