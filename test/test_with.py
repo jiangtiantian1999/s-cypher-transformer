dcf9713ab7cfb6cfe0ecc8c56ff67fac17abf387
CobaiN-why <717898675@qq.com>
2023-10-17 20:49:24 +0800

    todo : some test cases need to be add

--- test/test_with.py
+++ test/test_with.py
@@ -17,3 +17,11 @@ class TestWith(TestCase):
                    '\nRETURN name'
         cypher_query = transform_to_cypher(s_cypher)
         print("test_with_2:", '\n', s_cypher, '\n\n', cypher_query, '\n\n')
+
+## WITH date({year: 1984, month: 10, day: 11}) AS dd
+
+# 修改或转换结果：
+# 进行计算或使用函数：
+# 进行排序和限制：
+# 进行过滤
+# 多步骤查询中的连接
