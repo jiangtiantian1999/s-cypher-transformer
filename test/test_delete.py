33dd58b36727c8f4246eee55a67c4e20db4b2ddb
LingyaoLu <lingyaoluu@163.com>
2023-12-12 19:25:32 +0800

    update

--- test/test_delete.py
+++ test/test_delete.py
@@ -1,55 +1,70 @@
+from datetime import timezone
 from unittest import TestCase
 
+from neo4j.exceptions import ClientError
+from neo4j.time import DateTime
+
+from test.dataset_initialization import DataSet1
 from test.graphdb_connector import GraphDBConnector
 from transformer.s_transformer import STransformer
 
 
 class TestDelete(TestCase):
     graphdb_connector = None
+    dataset1 = None
 
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
         cls.graphdb_connector = GraphDBConnector()
         cls.graphdb_connector.out_net_connect()
+        cls.dataset1 = DataSet1(cls.graphdb_connector.driver)
 
     @classmethod
     def tearDownClass(cls) -> None:
         super().tearDownClass()
         cls.graphdb_connector.close()
 
-    def test_delete_1(self):
+    # 对象节点的物理删除
+    def test_delete_object_node(self):
         s_cypher = """
         MATCH (n {name: "Pauline Boutler"})
         DELETE n
         """
         cypher_query = STransformer.transform(s_cypher)
-        tx = self.graphdb_connector.driver.session().begin_transaction()
-        results = tx.run(cypher_query).data()
+        records, summery, keys = self.graphdb_connector.driver.execute_query(cypher_query)
+        # TODO assert
+        self.dataset1.rebuild()
 
-    def test_delete_2(self):
+    # 属性节点的物理删除
+    def test_delete_property_node(self):
         s_cypher = """
         MATCH (n {name: "Pauline Boutler"})
         DELETE n.name
         """
         cypher_query = STransformer.transform(s_cypher)
-        tx = self.graphdb_connector.driver.session().begin_transaction()
-        results = tx.run(cypher_query).data()
+        records, summery, keys = self.graphdb_connector.driver.execute_query(cypher_query)
+        # TODO assert
+        self.dataset1.rebuild()
 
-    def test_delete_3(self):
+    # 值节点的物理删除
+    def test_delete_value_node(self):
         s_cypher = """
         MATCH (n {name: "Pauline Boutler"})
         DELETE n.name#Value
         """
         cypher_query = STransformer.transform(s_cypher)
-        tx = self.graphdb_connector.driver.session().begin_transaction()
-        results = tx.run(cypher_query).data()
+        records, summery, keys = self.graphdb_connector.driver.execute_query(cypher_query)
+        # TODO assert
+        self.dataset1.rebuild()
 
-    def test_delete_4(self):
+    # 边的物理删除
+    def test_delete_relationship(self):
         s_cypher = """
         MATCH (a:Person {name: "Pauline Boutler"})-[e:LIVE_IN]->(b:City {name: "London"})
         DELETE e
         """
         cypher_query = STransformer.transform(s_cypher)
-        tx = self.graphdb_connector.driver.session().begin_transaction()
-        results = tx.run(cypher_query).data()
+        records, summery, keys = self.graphdb_connector.driver.execute_query(cypher_query)
+        # TODO assert
+        self.dataset1.rebuild()
