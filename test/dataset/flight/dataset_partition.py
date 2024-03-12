3d6ed843e55a4b520e510eaf3714fe7b7fd6e939
lingyaoluu <lingyaoluu@163.com>
2024-03-12 17:42:59 +0800

    update flight

--- test/dataset/flight/dataset_partition.py
+++ test/dataset/flight/dataset_partition.py
@@ -1,36 +1,79 @@
 import pandas as pd
+from datetime import datetime
 
-flight_df = pd.read_csv("flights.csv", dtype=str)
 
-flight_df["MONTH"] = flight_df["MONTH"].astype(int)
-flight_df["DAY"] = flight_df["DAY"].astype(int)
+def format_flight_data(date, flight_data):
+    print("Number of rows in " + date + ": ", len(flight_data))
+    # 剔除时间为空值的数据
+    flight_data = flight_data.dropna(
+        subset=["YEAR", "MONTH", "DAY", "DEPARTURE_TIME", "ARRIVAL_TIME", "FLIGHT_NUMBER", "TAIL_NUMBER"])
+    # 将 "YEAR"、"MONTH"、"DAY" 列转换为 datetime 对象
+    flight_data["DATE"] = flight_data.apply(
+        lambda date: datetime(int(date["YEAR"]), int(date["MONTH"].zfill(2)), int(date["DAY"].zfill(2))),
+        axis=1).dt.strftime("%Y-%m-%d")
 
-# # 提取第一周数据
-# first_week_data = flight_df[(flight_df["MONTH"] == 1) & (flight_df["DAY"] <= 7)]
-# first_week_data.to_csv("first_week.csv")
-# print("第一周数据行数：", len(first_week_data))
+    flight_data["DEPARTURE_TIME"] = flight_data["DEPARTURE_TIME"].str.zfill(4)
+    flight_data["DEPARTURE_TIME"] = flight_data["DEPARTURE_TIME"].str[:2] + ":" + flight_data["DEPARTURE_TIME"].str[2:]
+    # 处理24:00
+    flight_data["DEPARTURE_TIME"] = flight_data["DEPARTURE_TIME"].replace("24:00", "00:00")
+    flight_data["DEPARTURE_DATETIME"] = pd.to_datetime(flight_data["DATE"] + " " + flight_data["DEPARTURE_TIME"],
+                                                     format="%Y-%m-%d %H:%M")
 
-# # 提取第一月数据
-# first_month_data = flight_df[(flight_df["MONTH"] == 1)]
-# first_month_data.to_csv("first_month.csv")
-# print("第一月数据行数：", len(first_month_data))
+    flight_data["ARRIVAL_TIME"] = flight_data["ARRIVAL_TIME"].str.zfill(4)
+    flight_data["ARRIVAL_TIME"] = flight_data["ARRIVAL_TIME"].str[:2] + ":" + flight_data["ARRIVAL_TIME"].str[2:]
+    # 处理24:00
+    flight_data["ARRIVAL_TIME"] = flight_data["ARRIVAL_TIME"].replace("24:00", "00:00")
+    flight_data["ARRIVAL_DATETIME"] = pd.to_datetime(flight_data["DATE"] + " " + flight_data["ARRIVAL_TIME"],
+                                                   format="%Y-%m-%d %H:%M")
 
-# # 提取前三月数据
-# first_three_month_data = flight_df[(flight_df["MONTH"] <= 3)]
-# first_three_month_data.to_csv("first_three_month.csv")
-# print("前三月数据行数：", len(first_three_month_data))
-#
-# # 提取上半年数据
-# first_half_year_data = flight_df[(flight_df["MONTH"] <= 6)]
-# first_half_year_data.to_csv("first_half_year.csv")
-# print("上半年数据行数：", len(first_half_year_data))
+    # 处理不同时间的同一航班
+    flight_count = {}
 
-# # 提取全年数据
-# entire_year_data = flight_df[(flight_df["MONTH"] <= 12)]
-# entire_year_data.to_csv("first_entire_year.csv")
-# print("上半年数据行数：", len(entire_year_data))
+    def add_suffix(flight_number):
+        if flight_number in flight_count:
+            flight_count[flight_number] += 1
+            return f"{flight_number}_{flight_count[flight_number]}"
+        else:
+            flight_count[flight_number] = 1
+            return flight_number
 
-# 提取第一天数据
-first_day_data = flight_df[(flight_df["MONTH"] == 1) & (flight_df["DAY"] == 1)]
-first_day_data.to_csv("first_day.csv")
-print("第一天数据行数：", len(first_day_data))
+    # 重复航班号添加前缀
+    flight_data['SUFFIXED_FLIGHT_NUMBER'] = flight_data['FLIGHT_NUMBER'].apply(add_suffix)
+
+    # 写入修改后的数据到原文件
+    flight_data.to_csv("flight/" + date + "_clean.csv", index=False)
+    print("Load and format Flight data successfully.")
+
+
+if __name__ == "__main__":
+    date = "first_half_day"
+
+    flight_df = pd.read_csv("flights.csv", dtype=str)
+    flight_df["MONTH"] = flight_df["MONTH"].astype(int)
+    flight_df["DAY"] = flight_df["DAY"].astype(int)
+
+    if date == "first_half_day":
+        # 提取第一天上半天数据
+        data = flight_df[(flight_df["MONTH"] == 1) & (flight_df["DAY"] == 1) & (flight_df["DEPARTURE_TIME"] <= '1200')]
+    elif date == "first_day":
+        # 提取第一天数据
+        data = flight_df[(flight_df["MONTH"] == 1) & (flight_df["DAY"] == 1)]
+    elif date == "entire_year":
+        # 提取全年数据
+        data = flight_df[(flight_df["MONTH"] <= 12)]
+    elif date == "first_half_year":
+        # 提取上半年数据
+        data = flight_df[(flight_df["MONTH"] <= 6)]
+    elif date == "first_three_month":
+        # 提取前三月数据
+        data = flight_df[(flight_df["MONTH"] <= 3)]
+    elif date == "first_month":
+        # 提取第一月数据
+        data = flight_df[(flight_df["MONTH"] == 1)]
+    elif date == "first_week":
+        # 提取第一周数据
+        data = flight_df[(flight_df["MONTH"] == 1) & (flight_df["DAY"] <= 7)]
+    else:
+        data = flight_df
+
+    format_flight_data(date, data)
--- test/dataset/flight_dataset.py
+++ test/dataset/flight_dataset.py
@@ -3,7 +3,6 @@ from neo4j import BoltDriver
 from test.graphdb_connector import GraphDBConnector
 from transformer.s_transformer import STransformer
 import pandas as pd
-from datetime import datetime
 
 
 class FlightDataSet:
@@ -22,63 +21,29 @@ class FlightDataSet:
         cypher_query = STransformer.transform(s_cypher_query)
         self.driver.execute_query(cypher_query)
 
-    def initialize(self):
-        # 创建机场节点
+    # 创建机场节点
+    def create_airports(self):
         airports_df = pd.read_csv("flight/airports.csv", dtype=str)
         print("Load Airports data successfully.")
+        s_cypher_query = ""
         for index, airport in airports_df.iterrows():
             index_str = str(index)
-            s_cypher_query = ("CREATE (n" + index_str
+            s_cypher_query += ("CREATE (n" + index_str
                               + ":Airport{code: \"" + airport["IATA_CODE"]
                               + "\", name: \"" + airport["AIRPORT"]
                               + "\", city: \"" + airport["CITY"] + "\"})\n"
                               + "AT TIME datetime('1989')\n"
                               )
-            cypher_query = STransformer.transform(s_cypher_query)
-            self.driver.execute_query(cypher_query)
+        cypher_query = STransformer.transform(s_cypher_query)
+        self.driver.execute_query(cypher_query)
         print("Create Airport nodes successfully.")
 
-        # 创建航班边
-        # 剔除时间为空值的数据
-        # first_week, first_month, first_three_month, first_half_year, first_entire_year
-        date = "first_day"
-        flight_df = pd.read_csv("flight/" + date + ".csv", dtype=str).dropna(
-            subset=["YEAR", "MONTH", "DAY", "DEPARTURE_TIME", "ARRIVAL_TIME", "FLIGHT_NUMBER", "TAIL_NUMBER"])
-
-        # 将 "YEAR"、"MONTH"、"DAY" 列转换为 datetime 对象
-        flight_df["DATE"] = flight_df.apply(
-            lambda date: datetime(int(date["YEAR"]), int(date["MONTH"].zfill(2)), int(date["DAY"].zfill(2))),
-            axis=1).dt.strftime("%Y-%m-%d")
-
-        flight_df["DEPARTURE_TIME"] = flight_df["DEPARTURE_TIME"].str.zfill(4)
-        flight_df["DEPARTURE_TIME"] = flight_df["DEPARTURE_TIME"].str[:2] + ":" + flight_df["DEPARTURE_TIME"].str[2:]
-        # 处理24:00
-        flight_df["DEPARTURE_TIME"] = flight_df["DEPARTURE_TIME"].replace("24:00", "00:00")
-        flight_df["DEPARTURE_DATETIME"] = pd.to_datetime(flight_df["DATE"] + " " + flight_df["DEPARTURE_TIME"],
-                                                         format="%Y-%m-%d %H:%M")
-
-        flight_df["ARRIVAL_TIME"] = flight_df["ARRIVAL_TIME"].str.zfill(4)
-        flight_df["ARRIVAL_TIME"] = flight_df["ARRIVAL_TIME"].str[:2] + ":" + flight_df["ARRIVAL_TIME"].str[2:]
-        # 处理24:00
-        flight_df["ARRIVAL_TIME"] = flight_df["ARRIVAL_TIME"].replace("24:00", "00:00")
-        flight_df["ARRIVAL_DATETIME"] = pd.to_datetime(flight_df["DATE"] + " " + flight_df["ARRIVAL_TIME"],
-                                                       format="%Y-%m-%d %H:%M")
-
-        print("Load Flight data successfully.")
-
-        # 处理不同时间的同一航班
-        flight_count = {}
-
-        def add_suffix(flight_number):
-            if flight_number in flight_count:
-                flight_count[flight_number] += 1
-                return f"{flight_number}_{flight_count[flight_number]}"
-            else:
-                flight_count[flight_number] = 1
-                return flight_number
-
-        # Apply the function to create a new column with suffixed flight numbers
-        flight_df['SUFFIXED_FLIGHT_NUMBER'] = flight_df['FLIGHT_NUMBER'].apply(add_suffix)
+    # 创建航班边
+    def create_flights(self):
+        # first_half_day, first_day, first_week, first_month, first_three_month, first_half_year, first_entire_year
+        date = "first_half_day"
+        flight_df = pd.read_csv("flight/" + date + "_clean.csv", dtype=str)
+        print("Load Flights data successfully.")
 
         index_n_from = 1
         index_n_to = 2
@@ -91,27 +56,23 @@ class FlightDataSet:
                 s_cypher_query = (
                         "MATCH (n" + str(index_n_from) + ":Airport), (n" + str(index_n_to) + ":Airport)\n"
                         + "WHERE n" + str(index_n_from) + ".code = \"" + str(flight["ORIGIN_AIRPORT"])
-                        + "\" AND n" + str(index_n_to) + ".code = \"" + str(flight["DESTINATION_AIRPORT"])
-                        + "\"\n"
+                        + "\" AND n" + str(index_n_to) + ".code = \"" + str(flight["DESTINATION_AIRPORT"]) + "\"\n"
                         + "CREATE (n" + str(index_n_from) + ")-[e" + str(index_e) + ":F" + str(flight["SUFFIXED_FLIGHT_NUMBER"])
                         + "@T(\"" + str(flight["DEPARTURE_DATETIME"]) + "\", \"" + str(flight["ARRIVAL_DATETIME"])
                         + "\")]->(n" + str(index_n_to) + ")\n"
-                        + "SET e" + str(index_e) + ".tail_number = \"" + str(flight["TAIL_NUMBER"]) + "\"\n"
                 )
-
                 index_n_from += 2
                 index_n_to = index_n_from + 1
                 index_e += 1
 
-                with open('scypher.txt', 'a') as file:
-                    file.write(s_cypher_query)
-
                 cypher_query = STransformer.transform(s_cypher_query)
                 self.driver.execute_query(cypher_query)
-                with open('cypher.txt', 'a') as file:
-                    file.write(cypher_query)
 
         print("Create Flight relationships successfully.")
+
+    def initialize(self):
+        self.create_airports()
+        self.create_flights()
 
 
 graphdb_connector = GraphDBConnector()
